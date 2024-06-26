0d99424a6903ba8b47e6d5aef8840fdb40f25601
Sireesha Dadi <11dsiri@gmail.com>
2024-06-26 14:29:42 -0400

    basic info

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@
 import React from 'react';
 import './App.css';
 import Home from './components/Home';
+import Skills from './components/Skills';
 
 export const App: React.FC = ()=> {
 	return (
 		<>
       <Home/>	
+	  <Skills/>
     </>
 	);
 };
